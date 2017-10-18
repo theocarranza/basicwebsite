b3774a1a89159477f00274ff6cdaf3d11d4a8f90
Théo Carranza <theocarranza@gmail.com>
2017-10-18 17:33:51 -0300

    First monolithic commit
    Creates the basic structure of the frontend. More to come.

---
+++ frontend/src/app/skills/skills.service.ts
@@ -0,0 +1,20 @@
+import { Component, Input } from '@angular/core';
+import { Injectable } from '@angular/core';
+import { Http, Response, Headers, RequestOptions } from '@angular/http';
+import { Observable } from 'rxjs/Rx';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/operator/toPromise';
+import { Skills } from './skills.model';
+
+@Injectable()
+export class SkillsService {
+
+  constructor(private http: Http) { }
+
+  getList(): Promise<Skills[]> {
+    return this.http.get('.././assets/data/skills.json')
+      .toPromise()
+      .then(skills => skills.json())
+  }
+}
